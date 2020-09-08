1e3ec8c94f896fcc4d534c159239e72ca41e0079
EUNYOUNGYOON <milkoon1@naver.com>
2020-09-08 09:53:31 +0900

    - 가입하기 화면 생성 - 버튼 이벤트 생성 - Intente 생성

--- app/src/main/java/com/example/myappwithgit/JoinActivity.kt
+++ app/src/main/java/com/example/myappwithgit/JoinActivity.kt
@@ -2,10 +2,18 @@ package com.example.myappwithgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_join.*
 
 class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        cancelBtn.setOnClickListener {
+
+            // MainActivity에서 넘어온 Activity를 종료하고 싶으면
+            finish()
+        }
+
     }
 }
